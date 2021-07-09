75c5f1ab9244147220a3413ee1c5a829bf8c33a8
Patrick Coyle <36138086+patrickcoyle427@users.noreply.github.com>
2021-07-09 17:32:33 -0400

    Add files via upload

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,3 +1,20 @@
+'''
+AURegistrationBot - Written by Patrick Coyle
+
+Bot to assist with registration for side events and for reporting results for the
+main event of the Konami Remote Duel YCS, an online yugioh tournament played over
+webcam, faciilated by Discord.
+
+This was written specifically for this tournament and as is only functions on
+the Alternate Universes RDYCS discord server it was written for. To use it
+on your own server, you will need to update any of the guild and user IDs for
+for your own server and users. I have labeled anything that needs to be changed
+to do this.
+
+List of commands can be found at:
+https://github.com/patrickcoyle427/AU-Registration-Bot/blob/main/README.md
+'''
+
 from discord import Intents, Embed, File, DMChannel, Client
 from discord.ext.commands import Bot as BotBase
 from discord.ext.commands import CommandNotFound, CommandOnCooldown
